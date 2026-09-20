02f7564753b209b2c5f5b349fb81bb77f3c480cc
Joalison Ferreira <joalisonferreirad@gmail.com>
2026-09-20 01:33:43 -0300

    feat: fotos por imovel em pasta com nome e redirecionamento pos-compra
    - fotos de imoveis organizadas em imoveis/{slug}-{ID}/ sem quebrar o existente (move automatico no save)
    - compra de plano redireciona para obrigado-pela-assinatura

--- plugins/imovel-parceiro-core/imovel-parceiro-core.php
+++ plugins/imovel-parceiro-core/imovel-parceiro-core.php
@@ -49,6 +49,7 @@ class Imovel_Parceiro_Core {
         require_once IMOVEL_PARCEIRO_CORE_DIR . 'includes/class-partnership-workflow.php';
         require_once IMOVEL_PARCEIRO_CORE_DIR . 'includes/class-watermark.php';
         require_once IMOVEL_PARCEIRO_CORE_DIR . 'includes/class-property-visibility.php';
+        require_once IMOVEL_PARCEIRO_CORE_DIR . 'includes/class-property-media-organizer.php';
         require_once IMOVEL_PARCEIRO_CORE_DIR . 'includes/class-opportunities.php';
         require_once IMOVEL_PARCEIRO_CORE_DIR . 'includes/class-deals.php';
         require_once IMOVEL_PARCEIRO_CORE_DIR . 'includes/class-commissions.php';
